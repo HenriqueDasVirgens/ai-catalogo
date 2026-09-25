8666310deb3ced859e990ed222c32e56cf4e891f
HenriqueDasVirgens <henrique.virgens@capgemini.com>
2026-09-25 17:42:12 +0100

    Gemini model ocupado 3.5 lite

--- gemini_service.py
+++ gemini_service.py
@@ -8,7 +8,7 @@ client = genai.Client(
 def perguntar_gemini(pergunta: str):
 
     response = client.models.generate_content(
-        model="gemini-3.6-flash",
+        model="gemini-3.5-flash-lite",
         contents=pergunta
     )
 
